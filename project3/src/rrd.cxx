4c04029cac8aa3253f691d46db47523131f5dee5
Isaac Mooney <fs3383@wayne.edu>
2017-04-04 12:46:28 -0400

    Add files via upload
    It's alive!

---
+++ project3/src/rrd.cxx
@@ -0,0 +1,14 @@
+//  rrd.cpp
+//  Created by Isaac Mooney, Cody Grant, Samuel Doeg
+
+#include <iostream>
+#include "test.h"
+
+using namespace std;
+
+//implementation for relativistic resonance decays
+int main(int argc, char** argv) {
+    
+    cout << "Write me!\n";
+    return 0;
+}
---
+++ project3/src/test.h
@@ -0,0 +1,9 @@
+//  test.h
+//  Created by Isaac Mooney, Cody Grant, Samuel Doeg
+
+#ifndef test_h
+#define test_h
+
+#include <iostream>
+
+#endif /* test_h */
